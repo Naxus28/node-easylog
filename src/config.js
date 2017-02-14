6ff4f132f31b728fad0a4f942f9ac07c3fd2248e
Gabriel Ferraz <gabrielferraz27@gmail.com>
2017-02-13 23:47:27 -0500

    removes comments from config

--- src/config.js
+++ src/config.js
@@ -18,34 +18,16 @@ const exceptions = {
 
 // pretty error custom styles
 pe.appendStyle({
-   // this is a simple selector to the element that says 'Error'
    'pretty-error > header > title > kind': { background: 'none', color: 'red' },
-   // the 'colon' after 'Error':
    'pretty-error > header > colon': { display: 'none' },
-   // the error message
    'pretty-error > header > message': {
       color: 'red',
-      /* colors supported: black, red, green, yellow, blue, magenta, cyan, white,
-      * grey, bright-red, bright-green, bright-yellow, bright-blue,
-      * bright-magenta, bright-cyan, and bright-white
-      */
       background: 'none',
-      padding: '0 1' // top/bottom left/right
+      padding: '0 1' 
    },
    'pretty-error > trace > item': {
       marginLeft: 2,
       bullet: '"<grey>o</grey>"'
-      /* Notes on bullets:
-       * The string inside the quotation mark gets used as the character
-       * to show for the bullet point.
-       * You can set its color/background color using tags.
-       * This example sets the background color to white, and the text color
-       * to cyan, the character will be a hyphen with a space character
-       * on each side:
-       * example: '"<bg-white><cyan> - </cyan></bg-white>"'
-       * Note that we should use a margin of 3, since the bullet will be
-       * 3 characters long.
-       */
    },
    'pretty-error > trace > item > header > pointer > file': { color: 'bright-cyan'},
    'pretty-error > trace > item > header > pointer > colon': { color: 'cyan' },
