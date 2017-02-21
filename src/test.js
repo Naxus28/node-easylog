902de07736a036a93e044e5131904952811d9ccb
Gabriel Ferraz <gferraz@fullsail.com>
2017-02-21 17:40:00 -0500

    implements promises

---
+++ src/test.js
@@ -0,0 +1,7 @@
+var logger = require('./index');
+
+logger.log({'message': 'hello world', 'logType': 'success'});
+// logger.log({'message': 'hello world', 'logType': 'error'});
+// logger.log({'message': 'hello world', 'logType': 'notice'});
+// logger.log({'message': 'hello world', 'logType': 'warn'});
+// logger.log({'message': 'hello world', 'logType': 'succss'});
