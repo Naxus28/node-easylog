5ed71f44a6e5f24300b31714fb4313835974a16d
naxus28 <gabrielferraz27@gmail.com>
2017-02-23 22:20:34 -0500

    adds more calls to logger on 'test.js'

--- src/test.js
+++ src/test.js
@@ -2,6 +2,9 @@ var logger = require('./index');
 
 logger.log({'message': 'hello world'});
 logger.log({'logType': 'error'});
-// logger.log({'message': 'hello world', 'logType': 'notice'});
-// logger.log({'message': 'hello world', 'logType': 'warn'});
-// logger.log({'message': 'hello world', 'logType': 'success'});
+logger.log({'message': 'hello world', 'logType': 'error'});
+logger.log({'message': 'hello world', 'logType': 'notice'});
+logger.log({'message': 'hello world', 'logType': 'warn'});
+logger.log({'message': 'hello world', 'logType': 'success'});
+logger.log({'message': 'hello world', 'logType': 'succss'});
+logger.log({'mesage': 'hello world', 'logType': 'success'});
