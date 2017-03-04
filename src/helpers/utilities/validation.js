a3b1c0320a749026bcf72ee54baa7ad2017e138c
naxus28 <gabrielferraz27@gmail.com>
2017-03-04 17:05:18 -0500

    breaks test

--- src/helpers/utilities/validation.js
+++ src/helpers/utilities/validation.js
@@ -2,7 +2,7 @@ const config = require('../../config');
 
 let testObjKeys = (element, index, array) => config.logProperties.includes(element); 
 
-let objKeysValid = (objKeysArray) => objKeysArray.every(testObjKeys);
+let objKeysValid = (objKeysArray) => objKeysArray.every(teObjKeys);
 
 module.exports = {
 	objKeysValid
